dc3f071be750bdf7d184df9f3a091fbcb680316e
Jason Smith (FS) <jason.h.smith@gmail.com>
2025-08-20 01:51:36 -0500

    The latest test runner

--- run_real_api.py
+++ run_real_api.py
@@ -4,55 +4,61 @@
 import os
 import sys
 import json
-from chatfield import Interview, hint, must, reject, alice, bob, as_int, as_lang, as_float, as_set, as_list, as_dict, as_percent, as_bool
+from chatfield import Interview, hint, must, reject, alice, bob, as_int, as_str, as_lang, as_float, as_set, as_list, as_dict, as_percent, as_bool
+from chatfield import as_one, as_maybe, as_multi, as_any
 from chatfield import Interviewer
 
 import dotenv
 
-@bob("Product Owner")
-@bob.trait("Not deep technical, but has a clear vision of what they want")
-@alice("Expert Technology Consultant")
-@alice.trait("Technology partner for the Product Owner")
-@alice.trait("Needs to understand the request in detail to implement correctly")
-@alice.trait("Keeps things simple and focused without overwhelming the Product Owner")
-class TechWorkRequest(Interview):
-    """Product Owner's request for technical work"""
+# @bob("Product Owner")
+# @bob.trait("Not deep technical, but has a clear vision of what they want")
+# @alice("Expert Technology Consultant")
+# @alice.trait("Technology partner for the Product Owner")
+# @alice.trait("Needs to understand the request in detail to implement correctly")
+# @alice.trait("Keeps things simple and focused without overwhelming the Product Owner")
+# class TechWorkRequest(Interview):
+#     """Product Owner's request for technical work"""
     
-    @hint("A specific thing you want to build")
-    @must("specific enough to implement by a tech team")
-    @reject("data regulation environments HIPAA or SOC2")
-    def scope():
-        "Scope of Work"
+#     @hint("A specific thing you want to build")
+#     @must("specific enough to implement by a tech team")
+#     @reject("data regulation environments HIPAA or SOC2")
+#     def scope():
+#         "Scope of Work"
     
-    def optional_notes():
-        """Optional extra notes or context for the request."""
+#     def optional_notes():
+#         """Optional extra notes or context for the request."""
 
-    @as_bool
-    def enthusiasm():
-        "What is your enthusiasm level for this project"
+#     @as_bool
+#     def enthusiasm():
+#         "What is your enthusiasm level for this project"
 
-    @hint("What assets you have to work with, or what you already built, if any")
-    def current_status():
-        "Current status of the project"
+#     @hint("What assets you have to work with, or what you already built, if any")
+#     def current_status():
+#         "Current status of the project"
 
-    @reject("budget details or constraints")
-    def constraints():
-        "Project constraints, e.g. timeline, resources (excluding budget)"
+#     @reject("budget details or constraints")
+#     def constraints():
+#         "Project constraints, e.g. timeline, resources (excluding budget)"
 
-    @as_int
-    def number_of_users():
-        "Estimated number of users for the project"
+#     @as_int
+#     def number_of_users():
+#         "Estimated number of users for the project"
 
-    @must("specific amount of money")
-    @must("a specific time frequency, e.g. monthly, yearly")
-    @as_int("USD of project budget or -1 if unlimited budget")
-    def budget():
-        "Project budget"
+#     @must("specific amount of money")
+#     @must("a specific time frequency, e.g. monthly, yearly")
+#     @as_int("USD of project budget or -1 if unlimited budget")
+#     def budget():
+#         "Project budget"
 
-@bob('Man on the Street')
+# @bob('Man on the Street')
+# @bob('Man awaking from a coma')
 # @alice.trait('Talks like Medieval Knight')
 # @alice('Cliche Apple ][ Program')
-@alice('Kindergarten Teacher')
+# @alice("The Man's old Kindergarten Teacher")
+# @alice("Estranged Ex-Wife")
+# @alice('Circus Clown')
+@alice('Abraham Lincoln')
+# @alice.trait("writes in alternating limericks and haiku")
 class NotableNumbers(Interview):
     """Numbers important to you"""
 
@@ -67,19 +73,40 @@ class NotableNumbers(Interview):
     # @as_lang.fr
     # @as_lang.de
     # @as_lang.erlang_expression
-    # @as_lang.pig_latin
+    # @as_lang.pig_latin(f'pig latin and in all upper-case')
     # @as_lang.th
     # @as_lang.greek
-    @as_lang.traditional_chinese
+    # @as_lang.traditionalChinese
+    # @as_bool.even('True if the number is even, False if odd')
+    # @as_bool.odd('True if the number is odd, False if even')
+    # @as_bool.power_of_two('True if the number is a power of two, False otherwise')
+    # @as_choice.scale(f'Smaller than 10', '10 to 23', 'Larger than 23')
+    # @as_choice.spelling('starts with f when written longhand', 'starts with t when written longhand', 'all other numbers')
+    # @as_choice.prime('prime number', 'not a prime number')
+    # @as_choice.div4('div 4 remainder is 0', 'div 4 remainder is 1', 'div 4 remainder is 2', 'div 4 remainder is 3')
+    # @as_choice.user_feedback('feedback provided was numeric', 'feedback provided was prose or text')
+
+    @as_str('Timestamp in ISO format, representing this value number of minutes since Jan 1 2025 Zulu time')
+    @as_set.factors('The set of all factors the number, excluding 1 and the number itself')
+    @as_one.parity('even', 'odd')
+    @as_maybe.speaking('One syllable when spoken in English', 'Two syllables when spoken in English')
+    @as_multi.math_facts('even', 'odd', 'prime', 'composite', 'fibonacci', 'perfect square', 'power of two')
+    @as_any.other_facts('prime', 'mersenne prime', 'first digit is 3')
     def favorite():
         "what is your favorite number?"
-    # .as_french
-    # .as_lang.my_secret_pig_latin_dialect
     
     # @as_int
+    # @as_maybe.dimension('shoe', 'beverage', 'shirt', 'car')
+    # def size():
+    #     """What is your favorite size, and what thing does your size measure?"""
+    
+    # @as_int
+    # @as_int.negative('Negative of the favorite number')
+    # @as_int.random('Just any random in from 50 - 59')
     # @as_percent('Fraction of 100 except negative, e.g. disliking 13 implies -0.13')
-    def least_favorite():
-        "what is your least favorite number?"
+    # @as_lang.fr('In a long form French paragraph expounding on the number')
+    # def least_favorite():
+    #     "what is your least favorite number?"
 
 def main():
     dotenv.load_dotenv(override=True)
@@ -94,7 +121,6 @@ def interview_loop():
     print(f'Thread ID: {thread_id}')
     trace_url = """https://smith.langchain.com/o/92e94533-dd45-4b1d-bc4f-4fd9476bb1e4/projects/p/1991a1b2-6dad-4d39-8a19-bbc3be33a8b6?searchModel=%7B%22filter%22%3A%22and%28eq%28metadata_key%2C+%5C%22thread_id%5C%22%29%2C+eq%28metadata_value%2C+%5C%22{thread_id}%5C%22%29%29%22%7D&runtab=0&timeModel=%7B%22duration%22%3A%227d%22%7D&peek=0b7068bb-e9c6-4637-b5ca-b7f35d0689a7&peeked_trace=4d1b16a3-171c-4c08-99a9-d54d6a2e17bf"""
     trace_url = trace_url.format(thread_id=thread_id)
-    print(f'Trace: {trace_url}')
     interviewer = Interviewer(interview, thread_id=thread_id)
 
     user_input = None
@@ -119,9 +145,12 @@ def interview_loop():
         if interview.favorite is not None:
             print(f'Implied favorite float: {interview.favorite.as_float}')
         
-        if interview.least_favorite:
-            print(f'Least favorite number: {interview.least_favorite!r}')
-            print(f'Reciprocal of least favorite: {interview.least_favorite.as_float!r}')
+        # if interview.size:
+        #     print(f'Favorite size: {interview.size}')
+
+        # if interview.least_favorite:
+        #     print(f'Least favorite number: {interview.least_favorite!r}')
+        #     print(f'Reciprocal of least favorite: {interview.least_favorite.as_float!r}')
 
         if interview._done:
             print(f'Hooray! User request is done.')
@@ -145,6 +174,9 @@ def interview_loop():
     print(f'---------------------------')
     print(f"Dialogue finished:")
     print(interview._pretty())
+
+    print(f'')
+    print(f'Trace:\n{trace_url}')
 
 if __name__ == "__main__":
     main()
