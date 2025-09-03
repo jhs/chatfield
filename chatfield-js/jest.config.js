b59f997d8172f2af200465de96f8a4c55a52ea71
Jason Smith (FS) <jason.h.smith@gmail.com>
2025-09-03 09:00:36 -0500

    Use the correct test filenames

--- chatfield-js/jest.config.js
+++ chatfield-js/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: [
     '**/__tests__/**/*.ts',
-    '**/?(*.)+(spec|test).ts'
+    '**/test_*.ts'
   ],
   transform: {
     '^.+\\.ts$': ['ts-jest', {
